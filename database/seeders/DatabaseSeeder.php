d46d42e82c922149c60b23613f586612cf064bf4
Hubert Lipiński <hubertlipinskipl@gmail.com>
2022-06-12 20:05:04 +0200

    orchid

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Artisan::call('orchid:admin', [
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => 'password',
+        ]);
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
